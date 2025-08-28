71485a245072add65a3f6135b98c1a8be85473d0
Tim van den Dobbelsteen <timvdd@hotmail.com>
2025-08-28 14:09:49 +0200

    Add slides scaffolding and add them to slides.tsx

--- src/slides/slides.tsx
+++ src/slides/slides.tsx
@@ -1,5 +1,12 @@
 import { createSlide, createSlides, createTopic } from 'wb-slides';
 
+import Opdracht00 from './Opdrachten/00-setup-local.mdx';
+import Opdracht01 from './Opdrachten/01-override-text-field.mdx';
+import Opdracht02 from './Opdrachten/02-count-characters.mdx';
+import Opdracht03 from './Opdrachten/03-homepage-widget.mdx';
+import Opdracht04 from './Opdrachten/04-password-input.mdx';
+import Opdracht05 from './Opdrachten/05-application-label.mdx';
+import Opdracht06 from './Opdrachten/06-menu-hover-contrast.mdx';
 import Agenda from './Welkom/Agenda.mdx';
 import Goals from './Welkom/Goals.mdx';
 
@@ -9,6 +16,18 @@ export const slides = createSlides([
     slides: [
       createSlide({ title: 'Agenda', component: Agenda }),
       createSlide({ title: 'Goals', component: Goals }),
+    ],
+  }),
+  createTopic({
+    title: 'Assignments',
+    slides: [
+      createSlide({ title: 'Local setup', component: Opdracht00 }),
+      createSlide({ title: 'Override text field', component: Opdracht01 }),
+      createSlide({ title: 'Count characters', component: Opdracht02 }),
+      createSlide({ title: 'Homepage widget', component: Opdracht03 }),
+      createSlide({ title: 'Password input', component: Opdracht04 }),
+      createSlide({ title: 'Application label', component: Opdracht05 }),
+      createSlide({ title: 'Menu hover contrast', component: Opdracht06 }),
     ],
   }),
 ]);
